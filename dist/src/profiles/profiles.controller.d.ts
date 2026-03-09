ec6e8f26df3bd95881598b56bf369a17043a43e5
jungyunkwon <bayun_style@naver.com>
2026-03-09 14:25:06 +0900

    remove dist from git tracking

--- dist/src/profiles/profiles.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ProfilesService } from './profiles.service';
-export declare class ProfilesController {
-    private readonly profilesService;
-    constructor(profilesService: ProfilesService);
-    getMe(req: any): Promise<any>;
-    updateMe(req: any, updateData: any): Promise<any>;
-}
--- dist/src/profiles/profiles.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class ProfilesService {
-    private configService;
-    private supabase;
-    constructor(configService: ConfigService);
-    getMe(userId: string): Promise<any>;
-    updateMe(userId: string, updateData: any): Promise<any>;
-}
